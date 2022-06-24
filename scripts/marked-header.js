3d7f61c1adbf4bde6755dd540af7566333d17313
Masaaki Hijikata <30435850+hijimasa@users.noreply.github.com>
2022-06-24 11:46:14 +0900

    fixed header

--- scripts/marked-header.js
+++
@@ -1,6 +0,0 @@
-var renderer = new marked.Renderer();
-$(document).ready(function(){
-  $.get( "navigation.md", function( data ) {
-    $('#header').html(marked(data, { renderer: renderer }));
-  });
-});
