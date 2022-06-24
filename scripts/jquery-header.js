6eca2dd82857f771dd483a443881c3501493b022
Masaaki Hijikata <30435850+hijimasa@users.noreply.github.com>
2022-06-24 14:48:51 +0900

    fixed hidden contents

--- scripts/jquery-header.js
+++ scripts/jquery-header.js
@@ -3,7 +3,8 @@ $(function() {
         $header = $('header'),
         headerHeight = $header.outerHeight(),
         startPos = 0;
-  
+        $("body").css("margin-top", headerHeight + 10);//10pxだけ余裕をもたせる
+
     $win.on('load scroll', function() {
       var value = $(this).scrollTop();
       if ( value > startPos && value > headerHeight ) {
